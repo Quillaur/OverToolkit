d0b67401a846576e3a95973bda268d072b3b93b0
Oleg Samoylov <quillaur@outlook.com>
2016-08-12 19:45:40 +1000

    Add project files.

---
+++ OverToolkit/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverToolkit
+{
+    public class Class1
+    {
+    }
+}
