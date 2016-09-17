8eae3a040b68f44528ea85ec593c0f48f8c1c7fc
Oleg Samoylov <quillaur@outlook.com>
2016-09-17 17:16:55 +0300

    Added a class helper for localization.

---
+++ OverToolkit/Helpers/LocalizationHelper.cs
@@ -0,0 +1,26 @@
+﻿using Windows.ApplicationModel.Resources;
+
+namespace OverToolkit.Helpers
+{
+    /// <summary>
+    /// Класс-помощник для работы с локализацией.
+    /// </summary>
+    public static class LocalizationHelper
+    {
+        #region Private field
+        private static readonly ResourceLoader resourceLoader = ResourceLoader.GetForViewIndependentUse("Resources");
+        #endregion
+
+        #region Public method
+        /// <summary>
+        /// Получает строку из ресурсов *.resw.
+        /// </summary>
+        /// <param name="resourceName">Название элемента *.resw.</param>
+        /// <returns>Строка элемента *.resw.</returns>
+        public static string ToString(string resourceName)
+        {
+            return resourceLoader.GetString(resourceName);
+        }
+        #endregion
+    }
+}
