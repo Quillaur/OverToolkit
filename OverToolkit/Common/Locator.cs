2b51fbab99b363a66993c628d074506d2d075d77
Oleg Samoylov <quillaur@outlook.com>
2016-10-09 20:45:17 +0300

    Added a converter, a helper and the Locator.

---
+++ OverToolkit/Common/Locator.cs
@@ -0,0 +1,22 @@
+﻿namespace OverToolkit.Common
+{
+    /// <summary>
+    /// Представляет локатор помощника навигации. 
+    /// </summary>
+    public static class Locator
+    {
+        /// <summary>
+        /// Помощник навигации.
+        /// </summary>
+        public static INavigationHelper NavigationHelper { get; private set; }
+
+        /// <summary>
+        /// Инициализирует помощник навигации.
+        /// </summary>
+        /// <param name="navigationHelper">Экземпляр помощника навигации.</param>
+        public static void InitializeNavigationHelper(INavigationHelper navigationHelper)
+        {
+            NavigationHelper = navigationHelper;
+        }
+    }
+}
